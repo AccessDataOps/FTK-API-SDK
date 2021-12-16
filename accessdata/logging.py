dc8b1cef47662bb14358c346be89876968800787
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2021-12-16 11:05:10 +0000

    Updated logging module to support Linux based systems running CRON jobs.

--- accessdata/logging.py
+++ accessdata/logging.py
@@ -7,6 +7,7 @@
 from logging import basicConfig, getLogger, DEBUG
 from pathlib import Path
 from os import getlogin
+from sys import platform
 
 ##
 
@@ -14,7 +15,14 @@ __all__ = ("logger", "set_logging_level", )
 
 ##
 
-LOG_PATH = r"C:\ProgramData\AccessData\SDK\accessdata-sdk.log"
+if platform == "win32":
+	USER_NAME = getlogin()
+	LOG_PATH = r"C:\ProgramData\AccessData\SDK\accessdata-sdk.log"
+else:
+	from os import getuid
+	from pwd import getpwuid
+	USER_NAME = getpwuid(getuid()).pw_name
+	LOG_PATH = r"/var/log/accessdata-sdk.log"
 
 ##
 
@@ -22,7 +30,7 @@ logfile = Path(LOG_PATH)
 logfile.parent.mkdir(parents=True, exist_ok=True)
 logfile.touch(exist_ok=True)
 
-basicConfig(filename=logfile, format=getlogin() + ' - %(asctime)s - %(levelname)s:%(message)s')
+basicConfig(filename=logfile, format=USER_NAME + ' - %(asctime)s - %(levelname)s:%(message)s')
 logger = getLogger("accessdata")
 logger.setLevel(DEBUG)
 
